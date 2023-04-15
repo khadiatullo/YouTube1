d22931b873b3eb3dc98fc75528498090e8a6bf47
ваше_имя <_@email.com>
2023-04-15 18:47:53 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend:{
+
+      screens: {
+        'tablet': '700px',
+        // => @media (min-width: 700px) { ... }
+
+        'lgplus': '1200px',
+        // => @media (min-width: 1100px) { ... }
+
+        'sm1': '400px',
+        // => @media (min-width: 1100px) { ... }
+
+        
+      },
+
+      fontFamily: {
+        main:["Playfair Display"]
+      },
+      width:{
+        khadi:["31rem"]
+      },
+
+      
+    },
+  },
+
+  plugins: [],
+}
+
